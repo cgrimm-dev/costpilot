6e14da7f213018ef35cc75bb7d9104abc186b5e3
cgrimm-dev <cgr@etik.com>
2025-08-06 13:47:56 +0200

    Create states_pca.js

---
+++ states_pca.js
@@ -0,0 +1,13 @@
+let formularEl = null;
+
+export function setFormularEl(el) {
+  formularEl = el;
+}
+
+export function getFormularEl() {
+  return formularEl;
+}
+
+export function clearFormularEl() {
+  formularEl = null;
+}
